eb5658028f6ce45f844d1ffd3912fed6c5b772cd
Bill Vaughn <bill.vaughn@sealingtech.com>
2023-07-31 15:54:34 -0400

    vagrant tasks

--- ansible/library/vagrant.py
+++ ansible/library/vagrant.py
@@ -5,6 +5,7 @@ def run_module():
     module_args = dict(
         path=dict(type='str', required=True),
         command=dict(type='str', required=True),
+        provision=dict(type='boolean', required=False)
         output_box_name=dict(type='str', required=False),
     )
 
@@ -28,12 +29,21 @@ def run_module():
                 subprocess.check_call(['vagrant ', module.params['command'], '--output', module.params['output_box_name']], cwd=module.params['path'])
             else:
                 module.fail_json(msg='output_box_name is required for package command', **result)
-        elif module.params['command'] == 'up':
-            subprocess.check_call(['vagrant', module.params['command']])
         result['changed'] = True
         result['message'] = 'Vagrant' + module.params['command'] + ' executed successfully'
     except subprocess.CalledProcessError as e:
-        module.fail_json(msg='Vagrant' + module.params['command'] + ' failed', **result)
+        module.fail_json(msg='Vagrant ' + module.params['command'] + ' failed', **result)
+
+        if module.params['provision] == 'true':
+            if module.params['output_box_name']:
+                subprocess.check_call(['vagrant ', module.params['command'], '--provision'])
+            else:
+                module.fail_json(msg='Should be ran with up command', **result)
+        result['changed'] = True
+        result['message'] = 'Vagrant' + module.params['command'] + ' executed successfully'
+    except subprocess.CalledProcessError as e:
+        module.fail_json(msg='Vagrant ' + module.params['command'] + ' failed', **result)
+
 
     if module.params['command']:
         result['changed'] = True
